ae08522f2fbfbc00d4b008a63ef157045c33bc7c
Josue Garcia <josuegarcia1@cpp.edu>
2020-09-23 15:56:13 -0700

    Added the mongoose connection and connected the database

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.error(error);
+
+})
+
+db.once('open', () => console.log('Database successfully connected'));
 app.set('view engine', 'ejs'); 
 
 
